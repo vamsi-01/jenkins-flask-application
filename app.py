e14526f8c90cc06439f967efc158aa4535244459
Mathala Vamsi <66720568+vamsi-01@users.noreply.github.com>
2022-08-25 21:33:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1> Final Site Tata!!!</h1>'
+    return '<h1> Flask Application running on Ec2 Deployed by AWS CodeDeploy!!</h1>'
 
 app.run(host='0.0.0.0',port=5000)
